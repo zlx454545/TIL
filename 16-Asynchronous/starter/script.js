350fc2d238ad888d3eae3046f2cadf78df6635ac
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-07-19 15:55:41 +0900

    0719study
    0719study

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -198,14 +198,57 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 
 // whereAmI(52.508, 13.381);
 
+// console.clear();
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer', 0));
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 10000000000; i++) {}
+//   console.log(res);
+// });
+
+// console.log('Test end');
+
+// const lottery = new Promise(function (resolve, reject) {
+//   console.log('Lotter draw is happening');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve(3);
+//     } else {
+//       reject(new Error('You lost your money 💔'));
+//     }
+//   }, 2000);
+// });
+
+// lottery.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+Promise.resolve('abc').then(x => console.log(x));
+
 console.clear();
 
-console.log('Test start');
-setTimeout(() => console.log('0 sec timer', 0));
-Promise.resolve('Resolved promise 1').then(res => console.log(res));
-Promise.resolve('Resolved promise 2').then(res => {
-  for (let i = 0; i < 10000000000; i++) {}
-  console.log(res);
-});
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
 
-console.log('Test end');
+getPosition().then(pos => console.log(pos));
