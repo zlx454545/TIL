0cd9513240d36ee2689a5b19aad84e79ef6c54e3
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-07-24 20:55:59 +0900

    0724study
    0724study

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -468,32 +468,24 @@ Promise.race([
 Promise.allSettled([
   Promise.resolve('Success'),
   Promise.reject('ERROR'),
-  Promise.resolve('Another success'),
+  Promise.resolve('Success'),
 ]).then(res => console.log(res));
 
-Promise.all([
-  Promise.resolve('Success'),
-  Promise.reject('ERROR'),
-  Promise.resolve('Another success'),
-])
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
-
 // Promise.any [ES2021]
+
 Promise.any([
   Promise.resolve('Success'),
   Promise.reject('ERROR'),
-  Promise.resolve('Another success'),
+  Promise.resolve('Success'),
 ])
   .then(res => console.log(res))
   .catch(err => console.error(err));
 
-///////////////////////////////////////
-// Coding Challenge #3
-
 /* 
 PART 1
-Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Write an async function 'loadNPause' 
+that recreates Coding Challenge #2, 
+this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
 Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
 
 PART 2
@@ -507,82 +499,3 @@ TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn of
 
 GOOD LUCK 😀
 */
-
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
-
-const imgContainer = document.querySelector('.images');
-
-const createImage = function (imgPath) {
-  return new Promise(function (resolve, reject) {
-    const img = document.createElement('img');
-    img.src = imgPath;
-
-    img.addEventListener('load', function () {
-      imgContainer.append(img);
-      resolve(img);
-    });
-
-    img.addEventListener('error', function () {
-      reject(new Error('Image not found'));
-    });
-  });
-};
-
-let currentImg;
-
-// createImage('img/img-1.jpg')
-//   .then(img => {
-//     currentImg = img;
-//     console.log('Image 1 loaded');
-//     return wait(2);
-//   })
-//   .then(() => {
-//     currentImg.style.display = 'none';
-//     return createImage('img/img-2.jpg');
-//   })
-//   .then(img => {
-//     currentImg = img;
-//     console.log('Image 2 loaded');
-//     return wait(2);
-//   })
-//   .then(() => {
-//     currentImg.style.display = 'none';
-//   })
-//   .catch(err => console.error(err));
-
-// PART 1
-const loadNPause = async function () {
-  try {
-    // Load image 1
-    let img = await createImage('img/img-1.jpg');
-    console.log('Image 1 loaded');
-    await wait(2);
-    img.style.display = 'none';
-
-    // Load image 1
-    img = await createImage('img/img-2.jpg');
-    console.log('Image 2 loaded');
-    await wait(2);
-    img.style.display = 'none';
-  } catch (err) {
-    console.error(err);
-  }
-};
-// loadNPause();
-
-// PART 2
-const loadAll = async function (imgArr) {
-  try {
-    const imgs = imgArr.map(async img => await createImage(img));
-    const imgsEl = await Promise.all(imgs);
-    console.log(imgsEl);
-    imgsEl.forEach(img => img.classList.add('parallel'));
-  } catch (err) {
-    console.error(err);
-  }
-};
-loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
