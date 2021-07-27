f5ff378547e93117304b1b125e6f1a87c5d1c5d4
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-07-27 14:07:47 +0900

    parcel
    parcel

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -18,10 +18,10 @@
 
 // console.log(price);
 
-// import add, { cart } from './shoppingCart.js';
-// add('pizza', 2);
-// add('bread', 5);
-// add('apples', 4);
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
 // console.log(cart);
 
