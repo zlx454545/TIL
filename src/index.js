8e6fc5d2a07bdddb378d1d381b454abb61034c36
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-07-27 18:55:34 +0900

    ReactProps&Props_types
    Props&Props_types

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 // Component란 html을 반환하는 함수
 // 하나의 component만 rendering 할 수 있음
-// Component에 정보를 보낼 수 있음
+// Component에 정보를 보낼 수 있음 => props
 ReactDOM.render(
   <React.StrictMode>
     <App />
