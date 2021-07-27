7aa956dac4ef4f1714926d0f74717116d8738799
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-07-27 18:22:32 +0900

    Test again

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
+// Component란 html을 반환하는 함수
+// 하나의 component만 rendering 할 수 있음
+// Component에 정보를 보낼 수 있음
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
