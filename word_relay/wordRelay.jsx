cd0aa3c4de42e2dfb52a91f2f87df570f8da655e
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-02 22:36:59 +0900

    Add: 끝말잇기 컴포넌트 생성

--- word_relay/wordRelay.jsx
+++ word_relay/wordRelay.jsx
@@ -2,10 +2,49 @@ const React = require("react");
 
 class WordRelay extends React.Component {
   state = {
-    text: "Hello, webpack",
+    word: "박완섭",
+    value: "",
+    result: "",
   };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+      this.setState({ result: "딩동댕!", word: this.state.value, value: "" });
+      this.input.focus();
+    } else {
+      this.setState({ result: "땡!", value: "" });
+      this.input.focus();
+    }
+  };
+
+  onChangeInput = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  input;
+
+  onRefInput = (c) => {
+    this.input = c;
+  };
+
   render() {
-    return <h1>{this.state.text}</h1>;
+    return (
+      <>
+        <div>{this.state.word}</div>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            ref={this.onRefInput}
+            // value와 onchange는 세트!
+            // 그게 아니라면 defaultValue
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+          <button>입력!</button>
+        </form>
+        <div>{this.state.result}</div>
+      </>
+    );
   }
 }
 
