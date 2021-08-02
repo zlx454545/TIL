e5e0210f280871d922ecff494af2067fd1446d44
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-02 23:17:24 +0900

    Refactor: class를 hooks로 변경

--- wordRelay.jsx
+++ wordRelay.jsx
@@ -1,51 +1,50 @@
 const React = require("react");
+const { useState, useRef } = React;
 
-class WordRelay extends React.Component {
-  state = {
-    word: "박완섭",
-    value: "",
-    result: "",
-  };
+const WordRelay = () => {
+  const [word, setWord] = useState("박완섭");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
     e.preventDefault();
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({ result: "딩동댕!", word: this.state.value, value: "" });
-      this.input.focus();
+    if (word[word.length - 1] === value[0]) {
+      setWord(value);
+      setResult("딩동댕!");
+      setValue("");
+
+      inputRef.current.focus();
     } else {
-      this.setState({ result: "땡!", value: "" });
-      this.input.focus();
+      setResult("땡!");
+      setValue("");
+      inputRef.current.focus();
     }
   };
 
-  onChangeInput = (e) => {
-    this.setState({ value: e.target.value });
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
   };
 
-  input;
-
-  onRefInput = (c) => {
-    this.input = c;
-  };
-
-  render() {
-    return (
-      <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmitForm}>
-          <input
-            ref={this.onRefInput}
-            // value와 onchange는 세트!
-            // 그게 아니라면 defaultValue
-            value={this.state.value}
-            onChange={this.onChangeInput}
-          />
-          <button>입력!</button>
-        </form>
-        <div>{this.state.result}</div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="wordInput">글자를 입력하세요.</label>
+        <input
+          id="wordInput"
+          className="클래스입니다"
+          ref={inputRef}
+          // value와 onchange는 세트!
+          // 그게 아니라면 defaultValue
+          value={value}
+          onChange={onChangeInput}
+        />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
 
 module.exports = WordRelay;
