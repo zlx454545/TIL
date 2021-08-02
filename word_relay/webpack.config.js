e9855120336255df9beac37d4e7db91369c81c01
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-02 20:58:56 +0900

    Chore: 웹팩 설정, app.js 빌드

--- word_relay/webpack.config.js
+++ word_relay/webpack.config.js
@@ -1,1 +1,18 @@
-module.exports = {};
+const path = require("path");
+
+module.exports = {
+  name: "wordrelay-setting", // 큰 의미는 없다.
+  mode: "development", // 실서비스: production
+  devtool: "eval", // ??빠르게 하겠다는 의미
+  resolve: {
+    extensions: [".js", ".jsx"], // 확장자를 entry의 app에서 생략돼있을 경우
+    // 자동으로 매칭을 시켜준다.
+  },
+  entry: {
+    app: ["./client.jsx"],
+  }, // 입력
+  output: {
+    path: path.join(__dirname, "dist"), // ?? node기능. 경로지정해주는 함수
+    filename: "app.js",
+  }, // 출력
+};
