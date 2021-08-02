92fef40e325f432ad9c0158187137cba3f71d97b
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-03 00:24:28 +0900

    .

--- client2.jsx
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-const ReactDom = require("react-dom");
-
-const WordRelay = require("./numberBaseball");
-
-ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
