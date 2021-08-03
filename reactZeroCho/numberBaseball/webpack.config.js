97be24bf261588da69252df65e13ce36471d4629
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-03 16:18:19 +0900

    Chore: 핫 리로더 설정

--- reactZeroCho/numberBaseball/webpack.config.js
+++ reactZeroCho/numberBaseball/webpack.config.js
@@ -1,40 +1,58 @@
 const path = require("path");
 const webpack = require("webpack");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
-    name: "NumberBaseball-setting",
-    mode: "development",
-    devtool: "eval",
-    resolve: {
-        extensions: [".js", ".jsx"],
-    },
+  name: "NumberBaseball-setting",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 
-    entry: {
-        app: ["./client"],
-    },
+  entry: {
+    app: ["./client"],
+  },
 
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            loader: 'babel-loader',
-            options: {
-                presets: [['@babel/preset-env',{
-                    targets: {
-                        browsers: ['> 5% in KR'],
-                    }
-                }],"@babel/preset-react"],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            },
-        }],
-    },
-
-
-    plugins: [
-        new webpack.LoaderOptionsPlugin({debug: true}),
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 5% in KR"],
+                },
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
+        },
+      },
     ],
+  },
 
-    output:{
-        path: path.join(__dirname, "dist"), 
-        filename: "app.js",
-    }
+  plugins: [
+    new webpack.LoaderOptionsPlugin({ debug: true }),
+    new RefreshWebpackPlugin(),
+  ],
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+    publicPath: "/dist/",
+  },
+
+  devServer: {
+    publicPath: "/dist/",
+    hot: true,
+  },
 };
