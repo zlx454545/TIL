b409c4643c5315524b61c7d50435f4c083ac98e7
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-03 21:56:36 +0900

    Refactor: PureComponent, memo 정리

--- reactZeroCho/numberBaseball/Try.jsx
+++ reactZeroCho/numberBaseball/Try.jsx
@@ -1,15 +1,20 @@
 const React = require("react");
 
-const Try = ({ tryInfo, index }) => {
+// 7. hooks의 경우는 shouldComponentUpdate메소드도 존재하지 않고
+// PureComponenet도 존재하지 않음
+// 대신 memo를 사용할 수 있음
+// 8. 컴포넌트 위계에서 가장 끝에 위치한 자식의 경우 memo를 사용!
+// 자식들의 모두 memo를 사용한 컴포넌트의 경우도 memo 사용가능
+const Try = React.memo(({ tryInfo, index }) => {
   return (
     <li>
       <div>{tryInfo.try}</div>
       <div>{tryInfo.result}</div>
     </li>
   );
-};
+});
 
-// class Try extends React.Component {
+// class Try extends React.PureComponent {
 //   render() {
 //     return (
 //       <li>
