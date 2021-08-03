8bb8554ab476c27434f3953eda140a27594d68ed
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-04 00:19:46 +0900

    Refactor: 반응속도체크 class에서 hooks로 변경

--- reactZeroCho/response-check/src/ResponseCheck.js
+++ reactZeroCho/response-check/src/ResponseCheck.js
@@ -1,34 +1,136 @@
-import { Component } from "react";
+import { Component, useRef, useState } from "react";
+import React from "react";
 
-class ResponseCheck extends Component {
-  state = {
-    state: "waiting",
-    message: "클릭해서 시작하세요",
-    result: [],
+const ResponseCheck = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState("클릭해서 시작하세요");
+  const [result, setResult] = useState([]);
+  const timeOut = useRef(null);
+  const startTime = useRef(null);
+  const endTime = useRef(null);
+
+  const onClickScreen = () => {
+    if (state === "waiting") {
+      setState("ready");
+      setMessage("초록색이 되면 클릭하세요.");
+      timeOut.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금 클릭");
+        startTime.current = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === "ready") {
+      clearTimeout(timeOut.current);
+      setState("waiting");
+      setMessage("너무 성급하시군요! 초록색이 되고 클릭하세요.");
+    } else if (state === "now") {
+      endTime.current = new Date();
+      setState("waiting");
+      setMessage("클릭해서 시작하세요. ");
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current];
+      });
+    }
   };
 
-  onClickScreen = () => {};
+  const onReset = () => {
+    setResult([]);
+  };
 
-  // jsx 안에서는 조건문을 쓰지 않는다.
-  render() {
-    return (
+  const renderAverage = () => {
+    return result.length === 0 ? null : (
       <>
-        <div
-          id="screen"
-          className={this.state.state}
-          onClick={this.onClickScreen}
-        >
-          {this.state.message}
-        </div>
         <div>
-          평균 시간:{" "}
-          {this.state.result.reduce((acc, el) => acc + el, 0) /
-            this.state.result.length}
-          ms
+          평균 : {result.reduce((acc, el) => acc + el, 0) / result.length} ms{" "}
         </div>
+        <button onClick={onReset}>리셋</button>
       </>
     );
-  }
-}
+  };
+
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+};
+
+// class ResponseCheck extends Component {
+//   state = {
+//     state: "waiting",
+//     message: "클릭해서 시작하세요",
+//     result: [],
+//   };
+
+//   timeout;
+//   startTime;
+//   endTime;
+
+//   onClickScreen = () => {
+//     const { state, message, result } = this.state;
+//     if (state === "waiting") {
+//       this.setState({
+//         state: "ready",
+//         message: "초록색이 되면 클릭하세요.",
+//       });
+//       this.timeout = setTimeout(() => {
+//         this.setState({
+//           state: "now",
+//           message: "지금 클릭",
+//         });
+//         this.startTime = new Date();
+//       }, Math.floor(Math.random() * 1000) + 2000);
+//     } else if (state === "ready") {
+//       clearTimeout(this.timeout);
+//       this.setState({
+//         state: "waiting",
+//         message: "너무 성급하시군요! 초록색이 되고 클릭하세요.",
+//       });
+//     } else if (state === "now") {
+//       this.endTime = new Date();
+//       this.setState((prevState) => {
+//         return {
+//           state: "waiting",
+//           message: "클릭해서 시작하세요",
+//           result: [...prevState.result, this.endTime - this.startTime],
+//         };
+//       });
+//     }
+//   };
+
+//   onReset = () => {
+//     this.setState({
+//       result: [],
+//     });
+//   };
+
+//   renderAverage = () => {
+//     const { result } = this.state;
+//     return result.length === 0 ? null : (
+//       <>
+//         <div>
+//           평균 : {result.reduce((acc, el) => acc + el, 0) / result.length} ms{" "}
+//         </div>
+//         <button onClick={this.onReset}>리셋</button>
+//       </>
+//     );
+//   };
+
+//   // jsx 안에서는 조건문을 쓰지 않는다.
+//   render() {
+//     const { state, message } = this.state;
+//     return (
+//       <>
+//         <div id="screen" className={state} onClick={this.onClickScreen}>
+//           {message}
+//         </div>
+
+//         {this.renderAverage()}
+//       </>
+//     );
+//   }
+// }
 
 export default ResponseCheck;
