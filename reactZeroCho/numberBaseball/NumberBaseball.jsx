c32fde1ef983b6a42f50198ad16e6c86f49d910b
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-03 19:01:57 +0900

    Refactor: splice 관련 사소한 문법 처리

--- reactZeroCho/numberBaseball/NumberBaseball.jsx
+++ reactZeroCho/numberBaseball/NumberBaseball.jsx
@@ -7,8 +7,8 @@ function getNumbers() {
   const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const array = [];
   for (let i = 0; i < 4; i += 1) {
-    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-    array.push(chosen);
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1);
+    array.push(chosen[0]);
   }
   return array;
 }
