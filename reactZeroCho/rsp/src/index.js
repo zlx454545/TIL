24a669095515b26157501c4cf00b6c24dc2fcd9b
zlx454545 <76683390+zlx454545@users.noreply.github.com>
2021-08-05 17:11:40 +0900

    Create RSP

---
+++ reactZeroCho/rsp/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import RSP from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RSP />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
