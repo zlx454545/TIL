e3dc756795aa26eec2ed38d836ef16c99d8cf26d
zlx454545 <zlx454545@gmail.com>
2021-08-30 23:52:34 +0900

    0830

---
+++ react/reactVelopert/react-redux/src/App.js
@@ -0,0 +1,14 @@
+import Todos from "./components/Todos";
+import CounterContainer from "./containers/CounterContainer";
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
+  );
+};
+
+export default App;
