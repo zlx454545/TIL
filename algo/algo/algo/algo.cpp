3ce95897ac40fb76b75a1604a358dffc8b4eda25
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-09-17 21:27:55 +0900

    11650

--- algo/algo/algo/algo.cpp
+++ algo/algo/algo/algo.cpp
@@ -3,25 +3,26 @@
 #include<algorithm>
 using namespace std;
 
-vector<int> v;
+vector<pair<int, int>> v;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-    int n;
-    cin >> n;
+	int n;
+	cin >> n;
+
 	while (n--)
 	{
-		int x;
-		cin >> x;
-		v.push_back(x);
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x,y });
 	}
 
 	sort(v.begin(), v.end());
-
-	for (auto a : v) {
-		cout << a << "\n";
+    
+	for (auto p : v) {
+		cout << p.first << " " << p.second << "\n";
 	}
 }
