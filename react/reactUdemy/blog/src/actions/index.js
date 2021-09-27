e8e1bb4b88e21b649eb34b49518af5fd8b7c3814
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-09-27 22:54:04 +0900

    0927

--- react/reactUdemy/blog/src/actions/index.js
+++ react/reactUdemy/blog/src/actions/index.js
@@ -1,4 +1,15 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -11,3 +22,10 @@ export const fetchUser = (id) => async (dispatch) => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
