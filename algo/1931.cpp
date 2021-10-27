b4261da5c936ba0912d8061ababcb13dd6f8086f
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-27 17:31:16 +0900

    1931

---
+++ algo/1931.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+vector<pair<int, int>> v;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int ans = 0;
+	int now = 0;
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int start, end;
+		cin >> start >> end;
+		v.push_back({ end,start });
+	}
+
+	sort(v.begin(), v.end());
+
+	
+
+	for (auto x : v) {
+		if (x.second >= now) {
+			++ans;
+			now = x.first;
+		}
+	}
+
+	cout << ans;
+}
