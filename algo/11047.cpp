510bb7f679e1c39bfac0e414ae08073d0c9b6697
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-27 17:09:44 +0900

    11047

---
+++ algo/11047.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n; // 동전 종류
+	int k; // 동전 총합
+	vector<int> v;
+	int ans = 0;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	for (int i = v.size() - 1; i >= 0; i--) {
+		ans += k / v[i];
+		k = k % v[i];
+	}
+
+	cout << ans;
+}
