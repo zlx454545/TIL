e1fc24001433c3eae212b7edc038ffe614628e74
Wassup <76683390+zlx454545@users.noreply.github.com>
2021-10-27 17:08:43 +0900

    11047

--- algo/algo/algo/algo.cpp
+++ algo/algo/algo/algo.cpp
@@ -1,28 +1,33 @@
 ﻿#include <iostream>
 #include <vector>
-#include<algorithm>
 using namespace std;
 
-vector<pair<int, int>> v;
+
+
+
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n;
-	cin >> n;
+	int n; // 동전 종류
+	int k; // 동전 총합
+	vector<int> v;
+	int ans = 0;
 
-	while (n--)
-	{
-		int x, y;
-		cin >> x >> y;
-		v.push_back({ y,x });
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
 	}
 
-	sort(v.begin(), v.end());
-    
-	for (auto p : v) {
-		cout << p.second << " " << p.first << "\n";
+	for (int i = v.size() - 1; i >= 0; i--) {
+		ans += k / v[i];
+		k = k % v[i];
 	}
+
+	cout << ans;
 }
